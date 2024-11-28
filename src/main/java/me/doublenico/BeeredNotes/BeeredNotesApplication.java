c5ddc1bb208b084f3b72a5d028b1e9e71c1dbbf0
DoubleNico <tomescuvlad123@gmail.com>
2024-11-28 19:06:48 +0200

    Blank SpringBoot project

---
+++ src/main/java/me/doublenico/BeeredNotes/BeeredNotesApplication.java
@@ -0,0 +1,13 @@
+package me.doublenico.BeeredNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeredNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeredNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/doublenico/BeeredNotes/BeeredNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doublenico.BeeredNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeredNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
