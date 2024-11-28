708ebdcb78682ce0400f70e9a48538460d28606c
DoubleNico <tomescuvlad123@gmail.com>
2024-11-28 19:14:29 +0200

    Made UserModel and NoteModel

---
+++ src/main/java/me/doublenico/BeeredNotes/note/NoteModel.java
@@ -0,0 +1,18 @@
+package me.doublenico.BeeredNotes.note;
+
+public record NoteModel(String id, String title, String content) {
+    public NoteModel {
+        if (title.isBlank()) throw new IllegalArgumentException("Title cannot be blank");
+
+        if (content.isBlank()) throw new IllegalArgumentException("Content cannot be blank");
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+            "id='" + id + '\'' +
+            ", title='" + title + '\'' +
+            ", content='" + content + '\'' +
+            '}';
+    }
+}
---
+++ src/main/java/me/doublenico/BeeredNotes/user/UserModel.java
@@ -0,0 +1,29 @@
+package me.doublenico.BeeredNotes.user;
+
+import me.doublenico.BeeredNotes.note.NoteModel;
+
+import java.util.List;
+
+public record UserModel(String id, String username, String email, String password, List<NoteModel> notes) {
+    public UserModel {
+        if (username.isBlank())
+            throw new IllegalArgumentException("Username cannot be blank");
+
+        if (password.isBlank())
+            throw new IllegalArgumentException("Password cannot be blank");
+
+        if (email.isBlank())
+            throw new IllegalArgumentException("Email cannot be blank");
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "id='" + id + '\'' +
+            ", username='" + username + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            ", notes=" + notes +
+            '}';
+    }
+}
