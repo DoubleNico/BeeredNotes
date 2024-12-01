fb4bc89473dcba679141526315289cf57c1144e9
DoubleNico <tomescuvlad123@gmail.com>
2024-12-01 18:02:02 +0200

    Added GET,DELETE,POST to the Notes

--- src/main/java/me/doublenico/BeeredNotes/BeeredNotesApplication.java
+++ src/main/java/me/doublenico/BeeredNotes/BeeredNotesApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeeredNotesApplication {
 
 	public static void main(String[] args) {
+
 		Dotenv dotenv = Dotenv.configure().load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(BeeredNotesApplication.class, args);
